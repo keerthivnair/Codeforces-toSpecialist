a5f2c5d7cf238097099c4423bdaebaea47370bfa
Keerthi V Nair <keerthivnair2005@gmail.com>
2025-07-14 15:47:55 +0530

    day1 1200-1300

---
+++ day1/D_1709.cpp
@@ -0,0 +1,6 @@
+// see here i am seeing a abctrack or a dp situation the 
+// constraints are also less and each element
+// can move either be swaped to the side or be swapped vertically
+
+// see another question here that i ahve is that there are 2 arrays 
+// have to simulatneoudly think about both also so that how to manage and keep track
